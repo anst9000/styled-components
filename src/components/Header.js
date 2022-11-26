da16e59f2cb2f6d1acb6d19897e313c07eee9daf
Anders Strömberg <coltlaboy@yahoo.se>
2022-11-26 09:26:27 +0100

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import { Image, Logo, Nav, StyledHeader } from "./styles/Header.styled"
+import { Container } from "./styles/Container.styled"
+import { Button } from "./styles/Button.styled"
+import { Flex } from "./styles/Flex.styled"
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg' alt='logo' />
+          <Button>Try it free</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build the community your fans will love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+
+            <Button bg='#ff0099' color='#fff'>
+              Get Started For Free
+            </Button>
+          </div>
+
+          <Image src='./images/illustration-mockups.svg' alt='mockup' />
+        </Flex>
+      </Container>
+    </StyledHeader>
+  )
+}
+
+export default Header
